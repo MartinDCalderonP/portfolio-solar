aca9458b6c54f20f4bf29178c515f9dabbb1ac3a
Martín <martincalderon92@outlook.com>
2026-06-21 23:38:22 -0300

    feat: update year for Contienda Podcast project to 2023

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -49,7 +49,7 @@ export const planets: PlanetContent[] = [
         title: 'Contienda Podcast',
         description: 'Podcast de 3 episodios sobre la Primera, Segunda y la posibilidad de una Tercera Guerra Mundial. Proyecto grupal final para Taller de Expresión III (Cátedra: María Rosa Gómez). Idea original, locución, concepto y diseño general.',
         link: 'https://open.spotify.com/show/3GUR3yx5rygh92ARyanxdL',
-        year: 2022,
+        year: 2023,
       },
     ],
   },
